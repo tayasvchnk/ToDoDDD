e2944e4580bdbf97a489c244fe547d6712f643b9
tayasvchnk <tkhurduikova@gmail.com>
2023-05-06 16:24:28 +0600

    Создала классы в Entities

---
+++ UOW.DAL/Entities/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UOW.DAL.Entities
+{
+    public class Status
+    {
+        public string StatusName { get; set; } = null!;
+    }
+}
---
+++ UOW.DAL/Entities/Tasks.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UOW.DAL.Entities
+{
+    public class Tasks : BaseEntity
+    {
+        [Display(Name = "Название задачи")]
+        public string TaskName { get; set; } = null!;
+
+        [Display(Name = "Описание")]
+        public string Desc { get; set; } = null!;
+
+        [Display(Name = "Приоритет")]
+        public Guid PriorityGuid { get; set; }
+        public virtual Priority? Priority { get; set; }
+
+        [Display(Name = "Статус")]
+        public virtual Status? Status { get; set; }
+
+        [Display(Name = "Создать")]
+        public DateTime CreateDate { get; set; }
+    }
+}
